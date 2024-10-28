4ef2ce2787d4f273403c9f4be10dd6535f7f24e2
Julien-Gelles-Epitech <julien.gelles@epitech.eu>
2024-10-28 11:55:19 +0100

    login

--- diskdg/src/views/UserPage.vue
+++ diskdg/src/views/UserPage.vue
@@ -1,9 +1,21 @@
 <script setup>
-  import UserProfile from '../components/UserProfile.vue';
+import UserProfile from '../components/UserProfile.vue';
+import LoginPage from '../components/LoginPage.vue';
+import sessionApi from '../services/sessionApi.js';
+import { computed, watch } from 'vue';
+import { useSessionStore } from '../stores/session';
+
+const sessionStore = useSessionStore();
+const isAuthenticated = computed(() => sessionApi.isAuthenticated());
+
+watch(() => sessionStore.isConnected, () => {
+    isAuthenticated.value = sessionApi.isAuthenticated();
+});
 </script>
 
 <template>
   <div :class="['h-screen bg-blue-500 flex items-center justify-center', ]">
-    <UserProfile />
+    <UserProfile v-if="isAuthenticated" />
+    <LoginPage v-else />
   </div>
 </template>
